6e3674b9b2ccc13302e9aa1c7f215849f6cc2762
bendrendel <bfdrendel@gmail.com>
2021-03-29 21:50:26 -0600

    Changes based on solution

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 const showNavBtn = document.getElementById('show-nav-btn');
 const hideNavBtn = document.getElementById('hide-nav-btn');
-const navBottom = document.querySelector('.nav-bottom');
-const navMiddle = document.querySelector('.nav-middle');
-const navTop = document.querySelector('.nav-top');
+const navs = document.querySelectorAll('.nav');
 
-    
 showNavBtn.addEventListener('click', () => {
-    navBottom.classList.remove('hide');
-    navMiddle.classList.remove('hide');
-    navTop.classList.remove('hide');
+    navs.forEach(nav => nav.classList.remove('hide'));
 });
 
 hideNavBtn.addEventListener('click', () => {
-    navBottom.classList.add('hide');
-    navMiddle.classList.add('hide');
-    navTop.classList.add('hide');
-})
+    navs.forEach(nav => nav.classList.add('hide'));
+});
